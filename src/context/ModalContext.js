2bd613239080db608e23fe2c35e94cbf9b3567ab
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-07 12:54:42 +0200

    añadiendo la modal con la informacion

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,23 +1,36 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
+// crear el context
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
+  // state del provider
   const [idreceta, guardarIdReceta] = useState(null);
-  const [receta, guardarReceta] = useState({});
+  const [informacion, guardarReceta] = useState({});
 
+  // una vez que tenemos una receta, llamar la api
   useEffect(() => {
     const obtenerReceta = async () => {
       if (!idreceta) return;
+
       const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
       const resultado = await axios.get(url);
+
       guardarReceta(resultado.data.drinks[0]);
     };
     obtenerReceta();
   }, [idreceta]);
+
   return (
-    <ModalContext.Provider value={{ guardarIdReceta }}>
+    <ModalContext.Provider
+      value={{
+        informacion,
+        guardarIdReceta,
+        guardarReceta,
+      }}
+    >
       {props.children}
     </ModalContext.Provider>
   );
--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -12,21 +12,29 @@ const RecetasProvider = (props) => {
   const [consultar, guardarConsultar] = useState(false);
 
   const { nombre, categoria } = busqueda;
+
   useEffect(() => {
     if (consultar) {
       const obtenerRecetas = async () => {
         const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&c=${categoria}`;
+
         const resultado = await axios.get(url);
 
+        // console.log(resultado.data.drinks);
         guardarRecetas(resultado.data.drinks);
       };
+
       obtenerRecetas();
     }
   }, [busqueda]);
 
   return (
     <RecetasContext.Provider
-      value={{ recetas, buscarRecetas, guardarConsultar }}
+      value={{
+        recetas,
+        buscarRecetas,
+        guardarConsultar,
+      }}
     >
       {props.children}
     </RecetasContext.Provider>
